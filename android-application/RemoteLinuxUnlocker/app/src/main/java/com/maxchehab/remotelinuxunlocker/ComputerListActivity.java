2c419aefc1e953afabccdace7562e9de1e269f2b
maxchehab <maxchehab@gmail.com>
2017-07-13 00:23:22 -0700

    Design/layout for pairing module

--- android-application/RemoteLinuxUnlocker/app/src/main/java/com/maxchehab/remotelinuxunlocker/ComputerListActivity.java
+++ android-application/RemoteLinuxUnlocker/app/src/main/java/com/maxchehab/remotelinuxunlocker/ComputerListActivity.java
@@ -1,5 +1,7 @@
 package com.maxchehab.remotelinuxunlocker;
 
+import android.content.Intent;
+import android.content.SharedPreferences;
 import android.os.Bundle;
 import android.support.design.widget.FloatingActionButton;
 import android.support.design.widget.Snackbar;
@@ -7,10 +9,29 @@ import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
 
+import java.math.BigInteger;
+import java.security.SecureRandom;
+import java.util.Random;
+
 public class ComputerListActivity extends AppCompatActivity {
+
+    private static Random rnd = new Random();
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
+
+        SharedPreferences sharedPref = getSharedPreferences("data", MODE_PRIVATE);
+        if(!sharedPref.contains("key")){
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("key", new BigInteger(getRandomNumber(64)).toString());
+            editor.commit();
+        }
+        if(!sharedPref.contains("ips")){
+            SharedPreferences.Editor editor = sharedPref.edit();
+            editor.putString("ips", "");
+            editor.commit();
+        }
+
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_computer_list);
 
@@ -18,8 +39,8 @@ public class ComputerListActivity extends AppCompatActivity {
         fab.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
+                Intent intent = new Intent(view.getContext(), PairingActivity.class);
+                startActivity(intent);
             }
         });
 
@@ -34,6 +55,13 @@ public class ComputerListActivity extends AppCompatActivity {
 
         */
 
+    }
+
+    public static String getRandomNumber(int digCount) {
+        StringBuilder sb = new StringBuilder(digCount);
+        for(int i=0; i < digCount; i++)
+            sb.append((char)('0' + rnd.nextInt(10)));
+        return sb.toString();
     }
 
 }
---
+++ android-application/RemoteLinuxUnlocker/app/src/main/java/com/maxchehab/remotelinuxunlocker/PairingActivity.java
@@ -0,0 +1,46 @@
+package com.maxchehab.remotelinuxunlocker;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.text.InputFilter;
+import android.text.Spanned;
+import android.widget.EditText;
+
+public class PairingActivity extends AppCompatActivity {
+
+    private EditText ipInput;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_pairing);
+
+        ipInput = (EditText)findViewById(R.id.ipInput);
+        InputFilter[] filters = new InputFilter[1];
+        filters[0] = new InputFilter() {
+            @Override
+            public CharSequence filter(CharSequence source, int start,
+                                       int end, Spanned dest, int dstart, int dend) {
+                if (end > start) {
+                    String destTxt = dest.toString();
+                    String resultingTxt = destTxt.substring(0, dstart) +
+                            source.subSequence(start, end) +
+                            destTxt.substring(dend);
+                    if (!resultingTxt.matches ("^\\d{1,3}(\\." +
+                            "(\\d{1,3}(\\.(\\d{1,3}(\\.(\\d{1,3})?)?)?)?)?)?")) {
+                        return "";
+                    } else {
+                        String[] splits = resultingTxt.split("\\.");
+                        for (int i=0; i<splits.length; i++) {
+                            if (Integer.valueOf(splits[i]) > 255) {
+                                return "";
+                            }
+                        }
+                    }
+                }
+                return null;
+            }
+        };
+        ipInput.setFilters(filters);
+    }
+}
